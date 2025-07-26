0be198608ec95662b3ff1d9757bdb1db11026d5e
efondo-aaronkyle <akyleefondo@gmail.com>
2025-07-26 13:12:55 +0800

    initialized student info dataframe

--- efondo_aaronkyle.py
+++ efondo_aaronkyle.py
@@ -1,3 +1,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+student_info = {
+    'name': ["Kuromi", "Melody", "Cinnamoroll", "Hello Kitty"],
+    'course': ["IT", "Fine Arts", "Engineering", "Theatre Arts"],
+    'year_level': [3, 1, 4, 2],
+    'section': [1, 3, 5, 7],
+    'midterm': [1.00, 1.50, 1.75, 2.00],
+    'final': [1.00, 2.00, 1.50, 1.00]
+}
