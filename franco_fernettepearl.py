7c4d6cfa14b8632061ebdab698a3f3271b3c8112
franco-fernettepearl <fernettepearl.franco55@gmail.com>
2025-07-26 14:01:34 +0800

    create dataframe

--- franco_fernettepearl.py
+++ franco_fernettepearl.py
@@ -1,3 +1,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Creating DataFrame
+students = {'name': ["Kuromi", "Melody", "Cinnamoroll", "Hello Kitty"],
+            'course': ["IT", "Fine Arts", "Engineering", "Theatre Arts"],
+            'year-level': [3, 1, 4, 2],
+            'section': [1, 3, 5, 7],
+            'midterm': [100, 95, 92.5, 90],
+            'final': [100, 90, 95, 100]}
+df = pd.DataFrame(students)
