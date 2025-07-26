c66df32c5e744e8de3af81e9e79834db94265c69
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-07-26 13:19:09 +0800

    add given data

--- lopez_jakim.py
+++ lopez_jakim.py
@@ -1,3 +1,12 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+
+data = {
+    'Name': ['Kuromi', 'Melody', 'Cinnamoroll', 'Hello Kitty'],
+    'Course': ['IT', 'Fine Arts', 'Engineering', 'Theatre Arts'],
+    'Year Level': [3, 1, 4, 2],
+    'Section': [1, 3, 5, 7],
+    'Midterm': [100, 95, 92.5, 90],
+    'Final': [100, 90, 95, 100]
+}
